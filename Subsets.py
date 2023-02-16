317672835781afd4660f641944e4d70b4aafa151
Hotheadthing <125407576+Hotheadthing@users.noreply.github.com>
2023-02-16 08:29:10 +0530

    Update Subsets.py

--- Subsets.py
+++ Subsets.py
@@ -1,13 +1,15 @@
 nums = [1,2,3]
-nums = [3,2,4,1]
-res = [[]]
-for num in nums:
-    length = len(res)
-    # print(f"the lenght of res is {length}")
-    for i in range(length):
-        res.append(res[i] + [num])
-        # print(f"the valu of res is {res}")
+arr = []
+subset = []
 
-# print(res)
-print(2^3)
-print(bin(7)[2:])
+def dfs(i):
+    if i == len(nums):
+        arr.append(subset.copy())
+        return
+    subset.append(nums[i])
+    dfs(i+1)
+    subset.pop()
+    dfs(i+1)
+   
+dfs(0)
+return arr
